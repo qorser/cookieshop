57bb0dbb28e214dfdfab778236a7bb1dbf002ff9
elsanita <elsanita.sinaga@appschef.com>
2022-05-25 10:48:33 +0700

    add warning on So creation based on unpaid invoices

---
+++ preventing_so_based_on_payment/models/sale.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+from odoo.exceptions import AccessError, UserError, ValidationError
+
+class SaleOrder(models.Model):
+	_inherit = "sale.order"
+
+	def action_confirm(self):
+		# super().action_confirm(values)
+		super(SaleOrder, self).action_confirm()
+		if self.partner_id.is_restricted == True and int(self.partner_id.count_unpaid) > int(self.partner_id.max_unpaid) and self.partner_id.max_unpaid != False:
+			raise UserError('SO creation cannot be done because the unpaid invoices has reached the limit. Please make payment in advance.')
+
