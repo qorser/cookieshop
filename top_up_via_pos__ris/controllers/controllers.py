073570b001f48148bcea725edc6f5657619bca3f
elsanita <elsanita.sinaga@appschef.com>
2022-06-27 10:52:31 +0700

    fix error on PoS code and trx_id

--- top_up_via_pos__ris/controllers/controllers.py
+++ top_up_via_pos__ris/controllers/controllers.py
@@ -6,12 +6,12 @@ import requests
 class TopUpViaPosRis(http.Controller):
 
     @http.route('/isipulsa', type='json', auth='public')
-    def isipulsa(self, phone, id, pin, user, password):
+    def isipulsa(self, phone, id, pin, user, password, kode, trx_id):
         print('HALO INI CONTROLLER')      
         print(phone) 
         payload={}
         headers = {}
-        url = "http://103.119.55.59:8080/api/h2h?id="+str(id)+"&pin="+str(pin)+"&user="+str(user)+"&pass="+str(password)+"&kodeproduk="+str(product_code)+"&tujuan=08997927000&counter=1&idtrx="+str(IRS_trx_id)
+        url = "http://103.119.55.59:8080/api/h2h?id="+str(id)+"&pin="+str(pin)+"&user="+str(user)+"&pass="+str(password)+"&kodeproduk="+str(kode)+"&tujuan=08997927000&counter=1&idtrx="+str(trx_id)
         response = requests.request("GET", url, headers=headers, data=payload)
 
         print(response.text)
