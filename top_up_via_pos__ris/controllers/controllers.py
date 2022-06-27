20180e5506247956ef6c8a56fbd3436f3f767662
elsanita <elsanita.sinaga@appschef.com>
2022-06-27 10:41:02 +0700

    add product code and transacion id

--- top_up_via_pos__ris/controllers/controllers.py
+++ top_up_via_pos__ris/controllers/controllers.py
@@ -4,22 +4,6 @@ import requests
 
 
 class TopUpViaPosRis(http.Controller):
-#     @http.route('/top_up_via_pos__ris/top_up_via_pos__ris', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
-
-#     @http.route('/top_up_via_pos__ris/top_up_via_pos__ris/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('top_up_via_pos__ris.listing', {
-#             'root': '/top_up_via_pos__ris/top_up_via_pos__ris',
-#             'objects': http.request.env['top_up_via_pos__ris.top_up_via_pos__ris'].search([]),
-#         })
-
-#     @http.route('/top_up_via_pos__ris/top_up_via_pos__ris/objects/<model("top_up_via_pos__ris.top_up_via_pos__ris"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('top_up_via_pos__ris.object', {
-#             'object': obj
-#         })
 
     @http.route('/isipulsa', type='json', auth='public')
     def isipulsa(self, phone, id, pin, user, password):
@@ -27,7 +11,7 @@ class TopUpViaPosRis(http.Controller):
         print(phone) 
         payload={}
         headers = {}
-        url = "http://103.119.55.59:8080/api/h2h?id="+str(id)+"&pin="+str(pin)+"&user="+str(user)+"&pass="+str(password)+"&kodeproduk=T1&tujuan=08997927000&counter=1&idtrx=240622-1"
+        url = "http://103.119.55.59:8080/api/h2h?id="+str(id)+"&pin="+str(pin)+"&user="+str(user)+"&pass="+str(password)+"&kodeproduk="+str(product_code)+"&tujuan=08997927000&counter=1&idtrx="+str(IRS_trx_id)
         response = requests.request("GET", url, headers=headers, data=payload)
 
         print(response.text)
