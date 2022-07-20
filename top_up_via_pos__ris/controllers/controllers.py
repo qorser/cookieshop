a6b190c8ab80569889a69d715cbd6fbce41653a5
elsanita <elsanita.sinaga@appschef.com>
2022-07-20 14:58:25 +0700

    change counter2

--- top_up_via_pos__ris/controllers/controllers.py
+++ top_up_via_pos__ris/controllers/controllers.py
@@ -25,9 +25,11 @@ class TopUpViaPosRis(http.Controller):
                 return 'Tidak ada kode produk tersedia di database. Buat produk dengan kode internal: '+str(kode)
 
         if len(trx_type) > 0:
-            url = "http://103.119.55.59:8080/api/h2h?id="+str(id)+"&pin="+str(pin)+"&user="+str(user)+"&pass="+str(password)+"&kodeproduk="+str(product_code)+"&tujuan="+str(phone)+"&counter=2&idtrx="+str(trx_id)+"&jenis="+str(trx_type)
+            url = "http://103.119.55.59:8080/api/h2h?id="+str(id)+"&pin="+str(pin)+"&user="+str(user)+"&pass="+str(password)+"&kodeproduk="+str(product_code)+"&tujuan="+str(phone)+"&counter=3&idtrx="+str(trx_id)+"&jenis="+str(trx_type)
         else:
-            url = "http://103.119.55.59:8080/api/h2h?id="+str(id)+"&pin="+str(pin)+"&user="+str(user)+"&pass="+str(password)+"&kodeproduk="+str(product_code)+"&tujuan="+str(phone)+"&counter=2&idtrx="+str(trx_id)
+            url = "http://103.119.55.59:8080/api/h2h?id="+str(id)+"&pin="+str(pin)+"&user="+str(user)+"&pass="+str(password)+"&kodeproduk="+str(product_code)+"&tujuan="+str(phone)+"&counter=3&idtrx="+str(trx_id)
+
+        print(url)
 
         response = requests.request("GET", url, headers=headers, data=payload)
 
